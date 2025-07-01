eece689efd9cc89783662aeb3f49a6baff70018e
stenio medeiros <steniomedeiro85@gmail.com>
2025-07-01 16:07:52 -0300

    testes de integração para AlunoController e UsuarioController

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,64 @@
 package br.com.gerenciamento.controller;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import br.com.gerenciamento.model.Aluno;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveRetornarFormularioDeInsercaoDeAlunos() throws Exception {
+        mockMvc.perform(get("/inserirAlunos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/formAluno"))
+                .andExpect(model().attributeExists("aluno"));
+    }
+
+    @Test
+    public void deveRedirecionarParaListaAposInsercaoValida() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setNome("João da Silva");
+        aluno.setMatricula("123456");
+        aluno.setCurso(br.com.gerenciamento.enums.Curso.BIOMEDICINA);
+        aluno.setStatus(br.com.gerenciamento.enums.Status.ATIVO);
+        aluno.setTurno(br.com.gerenciamento.enums.Turno.MATUTINO);
+
+        mockMvc.perform(post("/InsertAlunos")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("nome", aluno.getNome())
+                .param("matricula", aluno.getMatricula())
+                .param("curso", aluno.getCurso().name())
+                .param("status", aluno.getStatus().name())
+                .param("turno", aluno.getTurno().name()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/alunos-adicionados"));
+    }
+
+    @Test
+    public void deveRetornarListaDeAlunos() throws Exception {
+        mockMvc.perform(get("/alunos-adicionados"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/listAlunos"))
+                .andExpect(model().attributeExists("alunosList"));
+    }
+
+    @Test
+    public void deveRetornarViewDeFiltroDeAlunos() throws Exception {
+        mockMvc.perform(get("/filtro-alunos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/filtroAlunos"));
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,79 @@
 package br.com.gerenciamento.controller;
 
-import org.junit.runner.RunWith;
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.repository.UsuarioRepository;
+import br.com.gerenciamento.service.ServiceUsuario;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ServiceUsuario serviceUsuario;
+
+    @BeforeEach
+    public void setup() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("teste@teste.com");
+        usuario.setUser("testeuser");
+        usuario.setSenha("123"); // senha será criptografada no método salvarUsuario
+        try {
+            serviceUsuario.salvarUsuario(usuario);
+        } catch (Exception e) {
+            // ignora erro de email duplicado
+        }
+    }
+
+    @Test
+    public void deveMostrarTelaDeLogin() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"))
+                .andExpect(model().attributeExists("usuario"));
+    }
+
+    @Test
+    public void deveMostrarTelaDeCadastro() throws Exception {
+        mockMvc.perform(get("/cadastro"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/cadastro"))
+                .andExpect(model().attributeExists("usuario"));
+    }
+
+    @Test
+    public void deveRedirecionarParaLoginAposCadastro() throws Exception {
+        mockMvc.perform(post("/salvarUsuario")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("email", "novo@exemplo.com")
+                .param("user", "usuarioNovo")
+                .param("senha", "senha123"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void deveRedirecionarParaIndexAposLoginValido() throws Exception {
+        mockMvc.perform(post("/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("user", "testeuser")
+                .param("senha", "123")) // senha em texto plano, será criptografada na lógica
+                .andExpect(status().isOk())
+                .andExpect(view().name("home/index"));
+    }
 }
